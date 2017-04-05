395d74fe9dfa9b17de57da84598880dcbcdd1d47
Piotr Gomoła <szkodniq@gmail.com>
2017-04-05 09:41:13 +0200

    Add eigen

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,13 @@
 #include <jni.h>
 #include <string>
 #include <opencv2/core.hpp>
+#include "Eigen/Eigen"
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_pl_poznan_put_orbslam_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
     std::string hello = "Hello from C++";
+    Eigen::Matrix2f matrix2f;
     return env->NewStringUTF(hello.c_str());
 }
 
