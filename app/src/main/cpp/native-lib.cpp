b16aa69657a30ed627372b41da3b6598e733edec
Piotr Gomoła <szkodniq@gmail.com>
2017-04-19 02:06:52 +0200

    Added eigen, g2o, boost a try to add dbow2

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,6 +16,7 @@ JNIEXPORT jstring JNICALL
 Java_pl_poznan_put_orbslam_MainActivity_validate(JNIEnv *env, jobject, jlong addrGray, jlong addrRgba) {
     cv::Rect();
     cv::Mat();
+    cv::Mat mat = cv::Mat::zeros(1, 1, CV_8U);
     std::string hello2 = "Hello from validate";
     return env->NewStringUTF(hello2.c_str());
 }
